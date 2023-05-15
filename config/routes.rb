58bb186fe3b73445c447880f3f454f3404b41672
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-15 16:45:07 -0300

    feat: adiciona rotas do tipo get para chefs_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/chefs", to: "chefs#index"
+
+  get "/chefs/:id", to: "chefs#show"
+
+  get "/chefs/:chef_id/address", to: "chefs#show_address"
+  get "/chefs/:chef_id/city", to: "chefs#show_city"
+  get "/chefs/:chef_id/state", to: "chefs#show_state"
+  get "/chefs/:chef_id/telephones", to: "chefs#show_telephones"
+  get "/chefs/:chef_id/dishes", to: "chefs#show_dishes"
+  # patch "/chefs/:id/", to: "chefs#update"
 end
