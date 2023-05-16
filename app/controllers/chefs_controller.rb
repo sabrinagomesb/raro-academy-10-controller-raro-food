eb88626babf2e3c8ae5dcbe0a2a175911df71ad8
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-16 01:02:49 -0300

    fix: faz pequenos ajustes no chefs_controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -9,7 +9,7 @@ class ChefsController < ApplicationController
   def show
     @chef = Chef.find(params[:id])
     render json: {
-      chef: @chef,
+      chef_id: @chef.id,
       name: @chef.user.name,
       cpf: @chef.user.cpf,
       email: @chef.user.email,
@@ -18,28 +18,57 @@ class ChefsController < ApplicationController
 
   def show_address
     chef = Chef.find(params[:chef_id])
-    render json: { address: chef.address }
+    render json: chef.address
   end
 
   def show_city
     chef = Chef.find(params[:chef_id])
-    render json: { city: chef.city }
+    render json: chef.city
   end
 
   def show_state
     chef = Chef.find(params[:chef_id])
-    render json: { state: chef.state }
+    render json: chef.state
   end
 
   def show_telephones
     chef = Chef.find(params[:chef_id])
-    render json: { telephones: chef.telephones }
+    render json: chef.telephones
   end
 
   def show_dishes
     chef = Chef.find(params[:chef_id])
-    render json: { dishes: chef.dishes }
+    render json: chef.dishes
   end
+
+  # /users method: post
+  # def create
+  #   @user = User.new(user_params)
+
+  #   if @user.save
+  #     render json: @user, status: :created
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+
+  #   @chef = Chef.new(
+  #     user: @user,
+  #     approver: Administrator.first,
+  #     address: Address.first,
+  #   )
+
+  #   if @chef.save
+  #     render json: @chef, status: :created
+  #   else
+  #     render json: @chef.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # private
+
+  # def chef_params
+  #   params.require(:chef).permit(:name, :email, :password, :cpf)
+  # end
 
   # def update
   #   @chef = Chef.find(params[:id])
