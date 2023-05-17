6a0ce1cc64da0b0536c1985f76a8734e4b05189b
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-17 00:07:32 -0300

    refactor: adiciona controller Chef

--- test/controllers/chefs_controller_test.rb
+++ test/controllers/chefs_controller_test.rb
@@ -2,7 +2,7 @@
 
 require 'test_helper'
 
-class ChefControllerTest < ActionDispatch::IntegrationTest
+class ChefsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
