dc30ec79a37b53a95f99ea385c952e3eb098444b
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-17 00:05:13 -0300

    refactor: adiciona controller telephone

---
+++ app/controllers/telephones_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class TelephonesController < ApplicationController
+  before_action :set_telephone, only: %i[show]
+  before_action :set_user, only: %i[index]
+
+  def index
+    telephones = @user ? @user.telephones : Telephones.all
+    render json: telephones
+  end
+
+  def show
+    render json: @telephones
+  end
+
+  private
+
+  def set_telephone
+    @telephones = Telephone.find(params[:id])
+  end
+
+  def set_user
+    return unless params[:chef_id] || params[:customer_id]
+
+    @user = params[:chef_id] ? Chef.find(params[:chef_id]) : Customer.find(params[:customer_id])
+  end
+end
