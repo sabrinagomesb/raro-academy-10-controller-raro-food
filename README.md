1157ab34ef0f363b4765085b0b57570e2b38e0c7
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-17 00:09:08 -0300

    refactor: adiciona rotas

--- README.md
+++ README.md
@@ -1,6 +1,109 @@
 # raro-food
 
-Projeto prático para aulas dos fundamentos de modelagem de banco de dados.
+## Routes checklist
+
+### Model Chef
+
+- /chefs
+  - [x] GET
+- /chefs/:id
+  - [x] GET
+- /chefs/:chef_id/address
+  - [x] GET
+- /chefs/:chef_id/city
+  - [x] GET
+- /chefs/:chef_id/state
+  - [x] GET
+- /chefs/:chef_id/telephones
+  - [x] GET
+- /chefs/:chef_id/dishes
+  - [x] GET
+
+### Model Coupon
+
+- /coupons
+  - [x] GET
+- /coupons/:id
+  - [x] GET
+- /coupons/:coupon_id/orders
+  - [x] GET
+
+### Model Customer
+
+- /customers
+  - [x] GET
+- /customers/:id
+  - [x] GET
+- /customers/:customer_id/orders
+  - [x] GET
+- /customers/:customer_id/addresses/:id
+  - [x] GET
+- /customers/:customer_id/telephones
+  - [x] GET
+- /customers/:customer_id/cards/:id
+  - [x] GET
+
+### Model Dish
+
+- /dishes
+  - [x] GET
+- /dishes/:id
+  - [x] GET
+- /dishes/:dish_id/categories
+  - [x] GET
+
+### Model Order
+
+- /orders
+  - [ ] GET
+- /orders/:id
+  - [ ] GET
+- /orders/:order_id/order_items
+  - [ ] GET
+- /orders/:order_id/order_items/:id
+  - [ ] GET
+- /orders/:order_id/dishes
+  - [ ] GET
+- /orders/:order_id/dishes/:id
+  - [ ] GET
+- /orders/:order_id/city
+  - [ ] GET
+- /orders/:order_id/payment
+  - [ ] GET
+
+### Model Payment
+
+- /payments
+  - [ ] GET
+- /payments/:id
+  - [ ] GET
+- /payments/:payment_id/customer
+  - [ ] GET
+
+### Model State
+
+- /states
+  - [x] GET
+- /states/:id
+  - [x] GET
+- /states/:state_id/cities
+  - [x] GET
+
+### EXTRAS
+
+### Model Telephone
+
+- /telephones
+  - [x] GET
+- /telephones/:id
+  - [x] GET
+
+### Model Address
+
+- /addresses
+  - [x] GET
+- /addresses/:id
+  - [x] GET
 
 ## setup
 
@@ -17,7 +120,7 @@ rails db:seed
 
 Para iniciar o servidor, recomenda-se a utilização do comando `./bin/dev`, pois ele garante os assets serão todos devidamente processados.
 
-#### Alguns comandos utilizados para nossa aplicação:
+#### Alguns comandos utilizados para nossa aplicação
 
 Verificar versão do ruby utilizada:
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,40 +2,87 @@
 
 Rails.application.routes.draw do
   # Model Chef:
-  get "/chefs", to: "chefs#index"
-  get "/chefs/:id", to: "chefs#show"
-  get "/chefs/:chef_id/address", to: "chefs#show_address"
-  get "/chefs/:chef_id/city", to: "chefs#show_city"
-  get "/chefs/:chef_id/state", to: "chefs#show_state"
-  get "/chefs/:chef_id/telephones", to: "chefs#show_telephones"
-  get "/chefs/:chef_id/dishes", to: "chefs#show_dishes"
-
-  #   Model Coupon:
-  get "/coupons", to: "coupons#index"
-  get "/coupons/:id", to: "coupons#show"
-  get "/coupons/:coupon_id/orders", to: "coupons#show_orders"
-
-  #   Model Customer:
-  resources :customers, :orders
-  resources :customers, only: %i[index show] do
-    resources :orders
+  resources :chefs, params: :chef_id, only: %i[index show] do           # /chefs
+    get 'city', to: 'chefs#city'                                        # /chefs/:chef_id/city
+    get 'state', to: 'states#index'                                     # /chefs/:chef_id/state
+    resources :dishes, only: %i[index show]                             # /chefs/:chef_id/dishes
+    resources :telephones, only: %i[index show]                         # /chefs/:chef_id/telephones
+    resources :addresses, path: 'address', only: %i[index show]         # /chefs/:chef_id/address
   end
-  get "/customers/:customer_id/addresses", to: "customers#show_addresses"
-  get "/customers/:customer_id/telephones", to: "customers#show_telephones"
-  get "/customers/:customer_id/cards", to: "customers#show_cards"
-  # todos os customers tem apenas um endereço
 
-  #   Model Dish:
-  get "/dishes", to: "dishes#index"
-  get "/dishes/:id", to: "dishes#show"
-  get "/dishes/:dish_id/categories", to: "dishes#show_categories"
-  # /dishes
-  # /dishes/:id
-  # /dishes/:dish_id/categories
+  # Model Customers:
+  resources :customers, params: :customer_id, only: %i[index show] do    # /customer
+    resources :orders, only: %i[index show]                              # /customer/:customer_id/dishes
+    resources :telephones, only: %i[index show]                          # /customer/:customer_id/telephones
+    resources :addresses, only: %i[index show]                           # /customer/:customer_id/addresses
+    resources :cards, only: %i[index show]                               # /customer/:customer_id/cards
+  end
 
-  #   Model Order:
-  get "/orders", to: "orders#index"
-  get "/orders/:id", to: "orders#show"
+  # Model Orders:
+  resources :orders, params: :order_id, only: %i[index show] # /orders
+
+  # Model Dishes:
+  resources :dishes, params: :dish_id, only: %i[index show] do          # /dishes
+    get 'categories', to: 'dishes#categories'                           # /dishes/:dish_id/categories
+  end
+  # Model States:
+  resources :states, params: :state_id, only: %i[index show] do
+    get 'cities', to: 'states#cities', as: 'state_cities' # /states/:state_id/cities
+  end
+
+  # Model Coupons:
+  resources :coupons, params: :coupon_id, only: %i[index show] do
+    resources :orders, only: %i[index] # /coupons/:coupon_id/orders
+  end
+
+  # Model Telephones:
+  resources :telephones, only: %i[index show] # /telephones
+
+  # Model Address:
+  resources :addresses, only: %i[index show] # /telephones
+
+  # resources :chefs, params: :chef_id, only: %i[index show] do
+  #   member do
+  #     resource :dishes, only: %i[index show create update destroy]
+  #   end
+  # end
+
+  # # Model Chef:
+  # get "/chefs", to: "chefs#index"
+  # get "/chefs/:id", to: "chefs#show"
+  # get "/chefs/:chef_id/address", to: "chefs#show_address"
+  # get "/chefs/:chef_id/city", to: "chefs#show_city"
+  # get "/chefs/:chef_id/state", to: "chefs#show_state"
+  # get "/chefs/:chef_id/telephones", to: "chefs#show_telephones"
+  # get "/chefs/:chef_id/dishes", to: "chefs#show_dishes"
+
+  # #   Model Coupon:
+  # get "/coupons", to: "coupons#index"
+  # get "/coupons/:id", to: "coupons#show"
+  # get "/coupons/:coupon_id/orders", to: "coupons#show_orders"
+
+  # #   Model Customer:
+  # resources :customers, :orders
+  # resources :customers, only: %i[index show] do
+  #   resources :orders
+  # end
+
+  # get "/customers/:customer_id/addresses", to: "customers#show_addresses"
+  # get "/customers/:customer_id/telephones", to: "customers#show_telephones"
+  # get "/customers/:customer_id/cards", to: "customers#show_cards"
+  # # todos os customers tem apenas um endereço
+
+  # #   Model Dish:
+  # get "/dishes", to: "dishes#index"
+  # get "/dishes/:id", to: "dishes#show"
+  # get "/dishes/:dish_id/categories", to: "dishes#show_categories"
+  # # /dishes
+  # # /dishes/:id
+  # # /dishes/:dish_id/categories
+
+  # #   Model Order:
+  # get "/orders", to: "orders#index"
+  # get "/orders/:id", to: "orders#show"
   # /orders
   # /orders/:id
   # /orders/:order_id/items
