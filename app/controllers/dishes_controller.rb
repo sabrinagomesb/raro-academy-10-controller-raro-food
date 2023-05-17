c39eb674603173aac1d2047c76311ef210061718
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-17 00:06:04 -0300

    refactor: adiciona controller Dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,16 +1,32 @@
+# frozen_string_literal: true
+
 class DishesController < ApplicationController
+  before_action :set_dish, only: %i[show]
+  before_action :set_chef, only: %i[index show]
+
   def index
-    @dishes = Dish.all
-    render json: @dishes
+    dishes = @chef ? @chef.dishes : Dish.all
+    render json: dishes
   end
 
   def show
-    @dish = Dish.find(params[:id])
-    render json: @dish
+    dish = @chef ? @chef.dishes.find(params[:id]) : @dish
+    render json: dish
   end
 
-  def show_categories
-    @dish = Dish.find(params[:dish_id])
-    render json: @dish.categories
+  def categories
+    render json: Dish.find(params[:dish_id]).categories
+  end
+
+  private
+
+  def set_dish
+    @dish = Dish.find(params[:id])
+  end
+
+  def set_chef
+    return unless params[:chef_id]
+
+    @chef = Chef.find(params[:chef_id])
   end
 end
