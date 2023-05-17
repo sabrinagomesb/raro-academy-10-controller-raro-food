4ec998860fd5451d29beea6091ae68741b013061
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-17 00:05:48 -0300

    refactor: adiciona controller Coupon

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 class CouponsController < ApplicationController
+  before_action :set_coupon, only: %i[show]
+
   def index
-    @coupons = Coupon.all
-    render json: @coupons
+    render json: Coupon.all
   end
 
   def show
-    @coupon = Coupon.find(params[:id])
     render json: @coupon
   end
 
   def show_orders
     coupon = Coupon.find(params[:coupon_id])
     render json: coupon.orders
+  end
+
+  private
+
+  def set_coupon
+    @coupon = Coupon.find(params[:id])
   end
 end
