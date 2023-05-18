075ad67a0a19e9e08c35e57eb743eed6bf2ef689
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-18 00:31:41 -0300

    refactor: refatora controller de Customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@
 
 class CustomersController < ApplicationController
   before_action :set_customer, only: %i[show update destroy]
-  before_action :set_user, only: %i[ update destroy]
+  before_action :set_user, only: %i[update destroy]
 
   def index
     @customers = Customer.all
@@ -10,15 +10,7 @@ class CustomersController < ApplicationController
   end
 
   def show
-    render json: {
-      customer_id: @customer.id,
-      name: @customer.user.name,
-      cpf: @customer.user.cpf,
-      email: @customer.user.email,
-      birthday: @customer.birthday,
-      created_at: @customer.created_at,
-      updated_at: @customer.updated_at
-    }, status: :ok
+    render json: customer_json(@customer), status: :ok
   end
 
   def cards
@@ -26,45 +18,20 @@ class CustomersController < ApplicationController
   end
 
   def create
-    user = User.new({
-                      name: customer_params[:name],
-                      email: customer_params[:email],
-                      password: customer_params[:password],
-                      cpf: customer_params[:cpf]
-                    })
-
-    customer = Customer.new({
-                              birthday: customer_params[:birthday],
-                              user:
-                            })
+    user = User.new(user_params)
+    customer = Customer.new(customer_params)
+    customer.user = user
 
     if user.save && customer.save
-      render json: { message: 'Customer successfully created', customer: }, status: :created
+      render json: { message: 'Customer successfully created', customer: customer_json(customer) }, status: :created
     else
       render json: { error: 'Error: customer not created', errors: user.errors }, status: :unprocessable_entity
     end
   end
 
   def update
-    user_params_input = {
-      name: customer_params[:name],
-      email: customer_params[:email],
-      password: customer_params[:password],
-      cpf: customer_params[:cpf]
-    }
-
-    customer_params_input = {
-      birthday: customer_params[:birthday]
-    }
-
-    if @user.update(user_params_input) && @customer.update(customer_params_input)
-      render json: { message: 'Customer successfully updated', customer: {
-        customer_id: @customer.id,
-        name: @customer.user.name,
-        cpf: @customer.user.cpf,
-        email: @customer.user.email,
-        birthday: @customer.birthday,
-      } }, status: :ok
+    if @user.update(user_params) && @customer.update(customer_params)
+      render json: { message: 'Customer successfully updated', customer: customer_json(customer) }, status: :ok
     else
       render json: { error: 'Error: customer not updated', errors: @user.errors }, status: :unprocessable_entity
     end
@@ -84,7 +51,23 @@ class CustomersController < ApplicationController
     @user = @customer.user
   end
 
+  def customer_json(customer)
+    {
+      customer_id: customer.id,
+      name: customer.user.name,
+      cpf: customer.user.cpf,
+      email: customer.user.email,
+      birthday: customer.birthday,
+      created_at: customer.created_at,
+      updated_at: customer.updated_at
+    }
+  end
+
   def customer_params
-    params.require(:customer).permit(:name, :email, :password, :birthday, :cpf)
+    params.require(:customer).permit(:birthday)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :cpf)
   end
 end
