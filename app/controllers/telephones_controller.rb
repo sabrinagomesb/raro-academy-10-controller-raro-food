2d3175f9a27f090ee1014cda586265dd81f0a1d8
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-17 21:26:05 -0300

    feat: adiciona rotas CRUD de Telephone

--- app/controllers/telephones_controller.rb
+++ app/controllers/telephones_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class TelephonesController < ApplicationController
-  before_action :set_telephone, only: %i[show]
-  before_action :set_user, only: %i[index]
+  before_action :set_telephone, only: %i[show destroy update]
+  before_action :set_user, only: %i[index create update]
 
   def index
     telephones = @user ? @user.telephones : Telephones.all
@@ -10,18 +10,54 @@ class TelephonesController < ApplicationController
   end
 
   def show
-    render json: @telephones, status: :ok
+    render json: @telephone, status: :ok
+  end
+
+  def create
+    telephone = Telephone.new(telephone_params)
+
+    if @user
+      telephone.contactable_id = @user.id
+      telephone.contactable_type = @user.class.name
+    end
+
+    if telephone.save
+      render json: { message: 'Telephone successfully created', telephone: }, status: :created
+    else
+      render json: { error: 'Error: telephone not created', errors: telephone.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user && @telephone.contactable_id != @user.id
+      render json: { error: 'Error: telephone not updated', errors: @telephone.errors }, status: :unprocessable_entity
+      return
+    end
+
+    if @telephone.update(telephone_params)
+      render json: { message: 'Telephone successfully updated', telephone: @telephone }, status: :ok
+    else
+      render json: { error: 'Error: telephone not updated', errors: @telephone.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    render json: { message: 'Successfully deleted' } if @telephone.destroy
   end
 
   private
 
   def set_telephone
-    @telephones = Telephone.find(params[:id])
+    @telephone = Telephone.find(params[:id])
   end
 
   def set_user
     return unless params[:chef_id] || params[:customer_id]
 
     @user = params[:chef_id] ? Chef.find(params[:chef_id]) : Customer.find(params[:customer_id])
+  end
+
+  def telephone_params
+    params.require(:telephone).permit(:number, :contactable_id, :contactable_type)
   end
 end
